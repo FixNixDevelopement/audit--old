502d10de40755bb4b2cb6162997c1a6d827e88c6
Ghayathri <aghayathri@hotmail.com>
2014-04-11 09:44:35 +0530

    Dashboard current user

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,8 @@
 class DashboardsController < ApplicationController
   def index
   	
-    @count_audit = current_user.auditinits.find_by_sql("select count(*) as count from auditinits")
-    
+    @count_audit = current_user.auditinits.where("user_id = ?" , current_user.id)
+
   end
 end
 
