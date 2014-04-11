4a4db800359a50bec34aaa664f0057cf5dd40934
Ghayathri <aghayathri@hotmail.com>
2014-04-11 09:31:04 +0530

    Dashboard- current_user

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,8 @@
 class DashboardsController < ApplicationController
   def index
   	
-    @count_audit = Auditinit.find_by_sql("select count(*) as count from auditinits")
-    @intern =Auditinit.find_by_sql("select a_type,count(*) as count from auditinits where a_type ='internal'")
-    @extern =Auditinit.find_by_sql("select a_type,count(*) as count from auditinits where a_type ='external'")
-    @intern1 =Auditinit.find_by_sql("select title,count(title) as share from auditinits where a_type ='internal'")
-    @extern1 =Auditinit.find_by_sql("select title,count(title) as share from auditinits where a_type ='external'")
-
+    @count_audit = current_user.auditinits.find_by_sql("select count(*) as count from auditinits")
+    
   end
 end
 
